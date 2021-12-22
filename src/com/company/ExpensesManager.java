e245863f6ca0ad6e4c5537f28310ecf657b31f8d
Nikita <example@mail.ru>
2021-12-22 23:33:20 +0300

    A

---
+++ src/com/company/ExpensesManager.java
@@ -0,0 +1,39 @@
+package com.company;
+public class ExpensesManager {
+    double[] expenses;
+
+    ExpensesManager(){
+        expenses = new double[7];
+    }
+
+    double saveExpense(double moneyBeforeSalary, double expense, int day) {
+        // Печать вопросов и считывание ответов оставьте в классе Praktikum
+
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    double findMaxExpense() {
+        double maxExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
+        }
+        return maxExpense;
+    }
+
+    void printAllExpenses() {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        }
+    }
+
+
+}
